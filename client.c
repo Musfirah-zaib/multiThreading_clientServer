c76371b5442c4a719c01e9a96d6126cd5e3db74c
Musfirah Zaib <146152378+Musfirah-zaib@users.noreply.github.com>
2026-01-18 21:28:18 +0500

    Refactor client.c for single-threaded string input
    Refactor client.c to remove multi-threading and reverse string functionality. Connect to server and send user input instead.

--- client.c
+++ client.c
@@ -1,62 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <pthread.h>
-
-void reverse(char str[]) {
-    int i, j;
-    char temp;
-    for (i = 0, j = strlen(str) - 1; i < j; i++, j--) {
-        temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
-    }
-}
-
-void* handle_client(void* arg) {
-    int client_sock = *(int*)arg;
-    char buffer[1024];
-
-    recv(client_sock, buffer, sizeof(buffer), 0);
-    reverse(buffer);
-    send(client_sock, buffer, strlen(buffer), 0);
-
-    close(client_sock);
-    pthread_exit(NULL);
-}
 
 int main() {
-    int sockfd, client_sock;
-    struct sockaddr_in server, client;
-    socklen_t len;
-    pthread_t tid;
+    int sockfd;
+    struct sockaddr_in server;
+    char buffer[1024];
 
-    // 1. Create socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // 2. Initialize server address
     server.sin_family = AF_INET;
     server.sin_port = htons(8080);
-    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    // 3. Bind
-    bind(sockfd, (struct sockaddr*)&server, sizeof(server));
+    connect(sockfd, (struct sockaddr*)&server, sizeof(server));
 
-    // 4. Listen
-    listen(sockfd, 5);
-    printf("Multi-threaded TCP Server waiting...\n");
+    printf("Enter string: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    buffer[strcspn(buffer, "\n")] = '\0';
 
-    while (1) {
-        len = sizeof(client);
-        client_sock = accept(sockfd, (struct sockaddr*)&client, &len);
+    send(sockfd, buffer, strlen(buffer), 0);
+    recv(sockfd, buffer, sizeof(buffer), 0);
 
-        // 5. Create thread
-        pthread_create(&tid, NULL, handle_client, &client_sock);
-        pthread_detach(tid);   // avoid zombie threads
-    }
-
+    printf("Result: %s\n", buffer);
     close(sockfd);
     return 0;
 }
